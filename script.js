39028ba30a70d8388513d21b21606bc0b3926c73
ZMCodi MSI Windows <zaim.mohsin@gmail.com>
2024-10-19 12:47:25 +0100

    change available change

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let price = 19.5;
 let cid = [
-  ['PENNY', 0.5],
-  ['NICKEL', 0 ],
-  ['DIME', 0],
-  ['QUARTER', 0],
-  ['ONE', 0],
-  ['FIVE', 0],
-  ['TEN', 0],
-  ['TWENTY', 0],
-  ['ONE HUNDRED', 0]
+  ['PENNY', 1.01],
+  ['NICKEL', 2.02],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 5],
+  ['FIVE', 55],
+  ['TEN', 50],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
 ];
 
 
